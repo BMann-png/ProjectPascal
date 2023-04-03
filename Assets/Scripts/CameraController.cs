d566f7c5ab2bb0f9b8e041b9ab1cf9a417b12e63
hababa2 <zactanpeterson@gmail.com>
2023-04-03 16:29:11 -0600

    Camera controlling, basic sprint

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
+	private static readonly float MAX_ANGLE = 90.0f;
+
 	[SerializeField] private float sensitivity;
 
-    void FixedUpdate()
+	private Transform player;
+
+	private void Start()
+	{
+		player = transform.parent;
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void FixedUpdate()
     {
-        
+		float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime;
+		float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime;
+
+		player.Rotate(Vector3.up, mouseX);
+
+		Quaternion yQuat = transform.localRotation * Quaternion.Euler(-mouseY, 0.0f, 0.0f);
+		if (Quaternion.Angle(Quaternion.identity, yQuat) < MAX_ANGLE) { transform.localRotation = yQuat; }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,24 +7,47 @@ public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private CharacterController controller;
 	[SerializeField] private float movementSpeed;
+	[SerializeField] private float sprintSpeed;
+
+	private new Transform camera;
+
+	private bool sprinting;
+	private float sprintTimer;
+	private float sprintCooldown;
 
 	private void OnValidate()
 	{
 		if(controller == null) { controller = GetComponent<CharacterController>(); }
+		camera = transform.GetChild(0);
 	}
 
 	private void FixedUpdate()
 	{
 		Vector3 movement = Vector3.zero;
 
-		if(Input.GetKey(KeyCode.W)) { movement.z += movementSpeed; }
-		if(Input.GetKey(KeyCode.S)) { movement.z -= movementSpeed; }
-		if(Input.GetKey(KeyCode.A)) { movement.x -= movementSpeed; }
-		if(Input.GetKey(KeyCode.D)) { movement.x += movementSpeed; }
+		sprintTimer -= Time.fixedDeltaTime;
+		sprintCooldown -= Time.fixedDeltaTime;
 
-		movement *= Time.fixedDeltaTime;
+		sprinting = sprintTimer > 0.0f;
 
-		//TODO: rotate with camera
+		if (Input.GetKey(KeyCode.LeftShift) && sprintCooldown <= 0.0f)
+		{
+			sprinting = true;
+			sprintTimer = 1.0f;
+			sprintCooldown = 10.0f;
+		}
+
+		if (sprinting)
+		{
+			movement = camera.forward * sprintSpeed * Time.fixedDeltaTime;
+		}
+		else
+		{
+			movement += camera.forward * Input.GetAxis("Vertical") * movementSpeed * Time.fixedDeltaTime;
+			movement += camera.right * Input.GetAxis("Horizontal") * movementSpeed * Time.fixedDeltaTime;
+		}
+
+		movement.y = 0.0f;
 
 		controller.Move(movement);
 
