2f033fe21215a59e96198736cb22e00cca7c02ea
Zach Peterson <zactanpeterson@gmail.com>
2023-04-04 16:09:32 -0600

    Better Sprinting Mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,49 +5,72 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] private float movementSpeed = 5.0f;
-	[SerializeField] private float sprintSpeed = 10.0f;
-	[SerializeField] private float sprintTime = 2.0f;
-	[SerializeField] private float sprintCooldown = 5.0f;
+	private static readonly float SPRINT_TIME = 3.0f;
+	private static readonly float SPRINT_COOLDOWN = 6.0f;
+	private static readonly float TRIP_TIME = 1.0f;
+	private static readonly float TRIP_PROBABILITY = 0.8f / SPRINT_TIME;
+	private static readonly float MOVEMENT_SPEED = 6.66f;
+	private static readonly float SPRINT_MOD = 1.5f;
+	private static readonly float TRIP_MOD = 0.5f;
 
 	private CharacterController controller;
+	private Vector3 movement;
 
-	private bool sprinting;
-	private float sprintTimer;
-	private float sprintCooldownTimer;
+	private bool sprinting = false;
+	private bool tripped = false;
+	private float sprintTimer = 0.0f;
+	private float sprintCooldownTimer = 0.0f;
+	private float tripTimer = 0.0f;
+
 
 	private void OnValidate()
 	{
-		if(controller == null) { controller = GetComponent<CharacterController>(); }
+		controller = GetComponent<CharacterController>();
+	}
+
+	private void FixedUpdate()
+	{
+		if (sprinting && Random.Range(0.0f, 1.0f) < (TRIP_PROBABILITY * Time.fixedDeltaTime))
+		{
+			tripped = true;
+			sprinting = false;
+			sprintTimer = 0.0f;
+			tripTimer = TRIP_TIME;
+		}
 	}
 
 	private void Update()
 	{
-		Vector3 movement = Vector3.zero;
+		movement = Vector3.zero;
+
+		float vertInput = Input.GetAxis("Vertical");
+		float HoriInput = Input.GetAxis("Horizontal");
 
 		sprintTimer -= Time.deltaTime;
 		sprintCooldownTimer -= Time.deltaTime;
+		tripTimer -= Time.deltaTime;
 
-		sprinting = sprintTimer > 0.0f;
+		tripped = tripTimer > 0.0f;
 
-		if (Input.GetKey(KeyCode.LeftShift) && sprintCooldownTimer <= 0.0f)
+		if (Input.GetKey(KeyCode.LeftShift) && sprintCooldownTimer <= 0.0f && vertInput > 0.0f && !sprinting)
 		{
 			sprinting = true;
-			sprintTimer = sprintTime;
-			sprintCooldownTimer = sprintCooldown;
+			sprintTimer = SPRINT_TIME;
+			sprintCooldownTimer = SPRINT_COOLDOWN;
 		}
 
-		if (sprinting)
+		if ((Input.GetKeyUp(KeyCode.LeftShift) || vertInput <= 0.0f || sprintTimer <= 0.0f) && sprinting)
 		{
-			movement = transform.forward * sprintSpeed * Time.deltaTime;
-		}
-		else
-		{
-			movement += transform.forward * Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-			movement += transform.right * Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+			if(sprintTimer > 0.0f) { sprintCooldownTimer -= sprintTimer; }
+			sprinting = false;
+			sprintTimer = 0.0f;
 		}
 
+		movement += transform.forward * vertInput * MOVEMENT_SPEED * Time.deltaTime * (sprinting ? SPRINT_MOD : 1.0f);
+		movement += transform.right * HoriInput * MOVEMENT_SPEED * Time.deltaTime;
 		movement.y = 0.0f;
+
+		movement *= tripped ? TRIP_MOD : 1.0f;
 
 		controller.Move(movement);
 
---
+++ Assets/Scripts/PlayerReceiver.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReceiver : MonoBehaviour
+{
+    //TODO: Receive packets from the network and set position/rotation
+    //TODO: Set animations as well
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
