fca7b0e5d4203e042f1ea41d1523bbb34b200aaf
Zach Peterson <zactanpeterson@gmail.com>
2023-04-21 15:14:04 -0600

    Cleanup

--- Assets/Scripts/Managers/LobbyHandler.cs
+++ Assets/Scripts/Managers/LobbyHandler.cs
@@ -48,18 +48,4 @@ public class LobbyHandler : MonoBehaviour
 	{
 		GameManager.Instance.SelectLevel(change);
 	}
-
-	public void CheckOwner()
-	{
-		if (!GameManager.Instance.IsServer)
-		{
-			startButton.SetActive(false);
-			levelSelect.SetActive(false);
-		}
-		else
-		{
-			startButton.SetActive(true);
-			levelSelect.SetActive(true);
-		}
-	}
 }
