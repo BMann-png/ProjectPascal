fcb668c2691be46e93644a2d8317fa791d4a9fb4
BMann-png <bmann@student.neumont.edu>
2023-04-26 16:46:18 -0600

    Destroy

--- Assets/Scripts/EnemyAI/States/DeathState.cs
+++ Assets/Scripts/EnemyAI/States/DeathState.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class DeathState : State
 {
+    float persist = 5.0f;
+
     public DeathState(string name, BaseAI agent) : base(name, agent) { }
 
     public override void OnEnter()
     {
         Debug.Log("Died");
         Agent.animator.SetTrigger("isDead");
+        GameObject.Destroy(Agent.gameObject, persist);
     }
 
     public override void OnExit()
