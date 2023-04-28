5daa85d0fc77b827e9a591ae593189ff836d5765
Peter Ouzts <pouzts@student.neumont.edu>
2023-04-28 13:07:38 -0600

    Fixed the prefab manager

--- Assets/Scripts/Managers/PrefabManager.cs
+++ Assets/Scripts/Managers/PrefabManager.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 public class PrefabManager : MonoBehaviour
 {
-	[SerializeField] private GameObject[] playerModels;
-	public GameObject[] PlayerModels { get => playerModels; }
+    [SerializeField] private GameObject[] playerModels;
+    public GameObject[] PlayerModels { get => playerModels; }
 
-	[SerializeField] private GameObject[] enemyModels;
-	public GameObject[] EnemyModels { get => enemyModels; }
+    [SerializeField] private GameObject[] enemyModels;
+    public GameObject[] EnemyModels { get => enemyModels; }
 
-	[SerializeField] private GameObject player;
-	public GameObject Player { get => player; }
+    [SerializeField] private GameObject[] specialModels;
+    public GameObject[] SpecialModels { get => specialModels; }
 
-	[SerializeField] private GameObject networkPlayer;
-	public GameObject NetworkPlayer { get => networkPlayer; }
+    [SerializeField] private GameObject player;
+    public GameObject Player { get => player; }
 
-	[SerializeField] private GameObject lobbyPlayer;
-	public GameObject LobbyPlayer { get => lobbyPlayer; }
-  
-	[SerializeField] private GameObject enemy;
-	public GameObject Enemy { get => enemy; }
+    [SerializeField] private GameObject networkPlayer;
+    public GameObject NetworkPlayer { get => networkPlayer; }
 
-	[SerializeField] private GameObject[] projectiles;
-	public GameObject[] Projectiles { get => projectiles; }
+    [SerializeField] private GameObject lobbyPlayer;
+    public GameObject LobbyPlayer { get => lobbyPlayer; }
 
-	[SerializeField] private GameObject[] networkProjectiles;
-	public GameObject[] NetworkProjectiles { get => networkProjectiles; }
+    [SerializeField] private GameObject enemy;
+    public GameObject Enemy { get => enemy; }
 
-	private void Awake()
-	{
-		DontDestroyOnLoad(gameObject);
-	}
+    [SerializeField] private GameObject networkEnemy;
+    public GameObject NetworkEnemy { get => networkEnemy; }
+
+    [SerializeField] private GameObject[] projectiles;
+    public GameObject[] Projectiles { get => projectiles; }
+
+    [SerializeField] private GameObject[] networkProjectiles;
+    public GameObject[] NetworkProjectiles { get => networkProjectiles; }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public GameObject RandomCommon()
+    {
+        return enemyModels[Random.Range(0, enemyModels.Length)];
+    }
 }
