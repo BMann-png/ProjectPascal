a9450e7a18aa22f960f050ba0edbc099da841cc9
BMann-png <bmann@student.neumont.edu>
2023-05-05 14:38:39 -0600

    Spawner adds

--- Assets/Scripts/Managers/MainMenuSpawner.cs
+++ Assets/Scripts/Managers/MainMenuSpawner.cs
@@ -9,12 +9,32 @@ public class MainMenuSpawner : Singleton<MainMenuSpawner>
 
 	public List<GameObject> playerLocations = new List<GameObject>();
 	private static readonly int MAX_ENEMY_COUNT = 15;
+	private int enemyCount = 0;
+
+	private GameObject[] spawners;
 
 	protected override void Awake()
 	{
 		base.Awake();
 
 		prefabManager = FindFirstObjectByType<PrefabManager>();
+
+		spawners = GameObject.FindGameObjectsWithTag("EnemySpawner");
+	}
+
+    private void Update()
+    {
+		if (enemyCount < MAX_ENEMY_COUNT)
+        {
+			GameObject spawner = spawners[Random.Range(0, spawners.Length)];
+			float x = Random.Range(-spawner.transform.localScale.x * 0.5f, spawner.transform.localScale.x * 0.5f);
+			float z = Random.Range(-spawner.transform.localScale.z * 0.5f, spawner.transform.localScale.z * 0.5f);
+			Transform t = gameObject.AddComponent<Transform>();
+			t.position = new Vector3(x, spawner.transform.position.y, z);
+			Instantiate(prefabManager.Enemy, t);
+			enemyCount++;
+        }
+
 	}
 
 
