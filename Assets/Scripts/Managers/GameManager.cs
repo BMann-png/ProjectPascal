bef3f239fc348ebd8ee1adbe90e5dcfc291e9f22
hababa2 <zactanpeterson@gmail.com>
2023-05-08 15:04:36 -0600

    Set EnemyCount

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -13,8 +13,8 @@ using UnityEngine.UIElements;
 public class GameManager : Singleton<GameManager>
 {
 	public List<GameObject> playerLocations = new List<GameObject>();
-	private static readonly int MAX_ENEMY_COUNT = 1;
-	private static readonly int MAX_SPECIAL_COUNT = 0;
+	private static readonly int MAX_ENEMY_COUNT = 30;
+	private static readonly int MAX_SPECIAL_COUNT = 2;
 	public static readonly ushort INVALID_ID = 65535;
 
 	public bool IsServer { get; private set; }
