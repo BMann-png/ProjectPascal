bc075951a651c1ae7ac3e0160e415dd8bc5cfb69
BMann-png <bmann@student.neumont.edu>
2023-05-19 15:57:10 -0600

    Children are now stolen when they die

---
+++ Assets/Scripts/Controllers/CaretakerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaretakerController : MonoBehaviour
+{
+    [SerializeField] Animator LeftAnimator;
+    [SerializeField] Animator RightAnimator;
+    [SerializeField] Transform startingPosition;
+    [SerializeField] Transform endPosition;
+    [SerializeField] GameObject hands;
+
+    private float startingCryTime;
+    private float startingDistanceFromChild;
+
+    private Animator playerAnimator;
+    private Health playerHealth;
+
+    private float RemainingHealth;
+
+    private void Start()
+    {
+        transform.position = startingPosition.position;
+        hands.SetActive(false);
+    }
+
+
+    private void Update()
+    {
+        if (playerAnimator == null)
+        {
+            playerAnimator = transform.GetComponentInParent<Entity>().animator;
+        }
+        if (playerHealth == null)
+        {
+            playerHealth = transform.GetComponentInParent<Entity>().GetComponent<Health>();
+        }
+        if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Flip"))
+        {
+            hands.SetActive(true);
+            transform.position = startingPosition.position;
+            startingCryTime = Time.time;
+            startingDistanceFromChild = (startingPosition.position - endPosition.position).magnitude;
+        }
+        else if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Crying"))
+        {
+            float distanceMoved = (100 - playerHealth.down) / 5;
+
+            float sectionOfDistance = distanceMoved / startingDistanceFromChild;
+
+            transform.position = Vector3.Lerp(startingPosition.position, endPosition.position, sectionOfDistance);
+        }
+        else if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Flip Back"))
+        {
+            hands.SetActive(false);
+        }
+        if (playerHealth.down <= 0)
+        {
+            SetAnimators();
+        }
+
+    }
+
+    private void SetAnimators()
+    {
+        LeftAnimator.SetTrigger("isDead");
+        RightAnimator.SetTrigger("isDead");
+    }
+
+    private void Revive()
+    {
+        Destroy(gameObject);
+    }
+}
