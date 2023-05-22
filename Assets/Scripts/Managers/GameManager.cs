cbaadd8ff69b1bf5ba0f3dac1af8a19c24a0be2c
Zach Peterson <59061026+zachqpeterson@users.noreply.github.com>
2023-05-22 16:25:53 -0600

    Particles (#52)
    * Level Load Fix
    
    * Nothin
    
    * Bubble is a Billboard
    
    * Imported Models
    
    * Pickup Models
    
    * Pickup Fix
    
    * Better Bubbles
    
    * Added Hand
    
    * Fixed Formatting
    
    * Can't Shoot While Sprinting
    
    * Added Bubbles
    
    * Projectile Particles
    
    * Added Network Projectiles
    
    * Water Projectile
    
    * Cotton ball stuff
    
    * Oops my bad
    
    * Enemy Hit Particles
    
    * Larger Pickup Hitbox
    
    ---------
    
    Co-authored-by: JoshCole31 <jcole@student.neumont.edu>
    Co-authored-by: BMann-png <bmann@student.neumont.edu>

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using static UnityEngine.UI.GridLayoutGroup;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -197,7 +196,7 @@ public class GameManager : Singleton<GameManager>
 
 					if (spawner.type < 100)
 					{
-						entities[id] = Instantiate(prefabManager.Pickups[spawner.type - 1], spawner.transform.position, spawner.transform.rotation).GetComponentInChildren<Entity>();
+						entities[id] = Instantiate(prefabManager.Pickups[spawner.type], spawner.transform.position, spawner.transform.rotation).GetComponentInChildren<Entity>();
 						entities[id].id = id;
 						Interactable inter = entities[id].GetComponent<Interactable>();
 						inter.SetEvents(spawner.onInteract, spawner.onStopInteract, spawner.onComplete);
@@ -486,13 +485,28 @@ public class GameManager : Singleton<GameManager>
 
 	public void PickupItem(byte id)
 	{
-		//0, 1 - bubble gun
-		//0, 0 - dart gun
-		//1, 0 - squirt gun
-		//2, 0 - pacifier
+		byte slot = 0;
+		byte type = 0;
 
-		//TODO: Not sure which weapons go in which slot
-		entities[ThisPlayer].GetComponent<Inventory>().SetWeapon(0, id);
+		switch(id)
+		{
+			case 0: slot = 0; type = 0; break;
+			case 1: slot = 0; type = 1; break;
+			case 2: slot = 1; type = 0; break;
+			case 3: slot = 1; type = 1; break;
+			case 4: slot = 2; type = 1; break;
+			case 5: slot = 3; type = 1; break;
+			default: break;
+		}
+
+		//0, 0 - dart gun
+		//0, 1 - bubble gun
+		//1, 0 - squirt gun
+		//1, 1 - ball gun
+		//2, 1 - pacifier
+		//2, 1 - mission
+
+		entities[ThisPlayer].GetComponent<Inventory>().SetWeapon(slot, type);
 	}
 
 	public void PushPlayer(Vector3 dir)
@@ -508,7 +522,7 @@ public class GameManager : Singleton<GameManager>
 
 			//Entity entity = entities[ThisPlayer];
 
-			Quaternion rotation = shoot.rotation * Quaternion.Euler(variation.x, 0, variation.y);
+			Quaternion rotation = shoot.rotation * Quaternion.Euler(variation.x, variation.y, 0.0f);
 
 			GameObject go = Instantiate(prefabManager.Projectiles[type], shoot.position, rotation);
 
